090a6609dd92a5f6ea3354affd2fe41c70374013
yamahare <yamanouehare@gmail.com>
2014-12-04 02:18:54 +0900

    Move .vimrc to vim and .zshenv to zsh

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-ln -sf ~/Dropbox/Home/vimrc ~/.vimrc
-ln -sf ~/Dropbox/Home/.zshenv ~/.zshenv
-ln -sf ~/Dropbox/Home/.ssh ~/.ssh
+ln -sf ~/Dropbox/Home/vim/.vimrc ~/.vimrc
+ln -sf ~/Dropbox/Home/zsh/.zshenv ~/.zshenv
+ln -sf ~/Dropbox/Home/.ssh/ ~/.ssh
 ln -sf ~/Dropbox/Home/.gitconfig ~/.gitconfig
